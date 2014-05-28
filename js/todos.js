18096a6bf579d3cdf073aa6da88d93f08e91da05
Ariel Jatib <ariel.jatib@gmail.com>
2014-05-28 17:06:40 -0400

    initial commit - completed ToDo list tutorial

---
+++ js/todos.js
@@ -0,0 +1,29 @@
+function TodoCtrl($scope) {
+	
+	$scope.todos = [
+		{text:'Learning AngularJS', done:false},
+		{text:'Build an app', done:false}
+	];
+
+	$scope.getTotalTodos = function () {
+		return $scope.todos.length;
+	}
+
+	$scope.addTodo = function () {
+		console.log('added todo');
+		$scope.todos.push({text:$scope.formTodoText, done:false});
+		$scope.formTodoText = '';
+	}
+
+	$scope.clearCompleted = function () {
+		console.log('hit Clear Completed function');
+		//$scope.todos = _.filter($scope.todos, function (todo) 
+		$scope.todos = $scope.todos.filter(function(todo){
+			return !todo.done;
+		})
+	}
+}
+
+function halo() {
+	document.write('hello world')
+	}
